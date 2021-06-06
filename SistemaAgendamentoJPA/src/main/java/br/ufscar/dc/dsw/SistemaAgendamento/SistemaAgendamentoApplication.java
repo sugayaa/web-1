d08e1743bc6409ca119401a67f93ad9e7f7c98fb
sugaya <vitorsugaya5@gmail.com>
2021-06-06 16:50:35 -0300

    [JPA] directory and configs

---
+++ SistemaAgendamentoJPA/src/main/java/br/ufscar/dc/dsw/SistemaAgendamento/SistemaAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.SistemaAgendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAgendamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAgendamentoApplication.class, args);
+	}
+
+}
