7292576ed03b03bdaf6edcfb88e1c368a399cf2d
sugaya <vitorsugaya5@gmail.com>
2021-06-06 18:02:18 -0300

    [Domain] Consulta

---
+++ SistemaAgendamentoJPA/src/main/java/br/ufscar/dc/dsw/domain/Consulta.java
@@ -0,0 +1,120 @@
+package br.ufscar.dc.dsw.domain;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.NumberFormat.Style;
+
+@Entity
+@Table(name = "Consulta")
+public class Consulta {
+
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = false, length = 60)
+    private String nome;
+
+    @Column(nullable = false, length = 19)
+    private String data;
+
+    @Column(nullable = false, length = 15)
+    private String horario;
+
+    @Column(nullable = false, length = 30)
+    private String url;
+
+    @ManyToOne
+    @JoinColumn(name = "editora_id")
+    private Editora editora;
+
+    @NotNull(message = "{NotNull.consulta.cliente}")
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @NotNull(message = "{NotNull.consulta.profissional}")
+    @ManyToOne
+    @JoinColumn(name = "profissional_id")
+    private Profissional profissional;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getProfissional() {
+        return profissional;
+    }
+
+    public void setProfissional(String profissional) {
+        this.profissional = profissional;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("[");
+        sb.append("Nome: " + nome + ", ");
+        sb.append("data: " + data + ", ");
+        sb.append("horario: " + horario + ", ");
+        sb.append("url: " + url + ", ");
+        sb.append("cliente: " + cliente + ", ");
+        sb.append("profissional: " + profissional + ", ");
+        sb.append("]");
+        return sb.toString(); 
+    }
+
+}
